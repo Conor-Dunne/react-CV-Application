57a6b0644022e5752bb84dc9b5346c339d1a8bfc
Conor <conordunne@outlook.ie>
2022-12-22 04:48:17 +0000

    Add save function

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,4 +1,3 @@
-import { render } from "@testing-library/react";
 import React, { Component } from "react";
 
 class Personal extends Component {
@@ -8,9 +7,11 @@ class Personal extends Component {
       name: "",
       email: "",
       phoneNumber: "",
+      isSaved: false
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(event) {
@@ -23,35 +24,67 @@ class Personal extends Component {
     });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      isSaved: true
+    })
+    console.log(this.state.isSaved);
+  }
+
+  
+
   render() {
-    return (
-      <div style={{ border: "1px solid black", padding: "20px" }}>
-        <label htmlFor="name">Name: </label>
-        <input
-         name="name"
-         type="text" 
-         value={this.state.name} 
-         onChange={this.handleInputChange}
-         />
-         <label htmlFor="email">email: </label>
-         <input
-         name="email"
-         type="email" 
-         value={this.state.email} 
-         onChange={this.handleInputChange}
-         />
-         <label htmlFor="phoneNumber">Phone number: </label>
-         <input
-         name="phoneNumber"
-         type="tel" 
-         value={this.state.phoneNumber} 
-         onChange={this.handleInputChange}
-         />
-         <p>{this.state.name}</p>
-         <p>{this.state.email}</p>
-         <p>{this.state.phoneNumber}</p>
-      </div>
-    );
+    
+    if(!this.state.isSaved) {
+      return (
+        <div style={{ border: "1px solid black"}}>
+         <form onSubmit={this.handleSubmit}>
+          <div className="inputs-container">
+          <div className="inputBox">
+          <label htmlFor="name">Name: </label>
+          <input
+           name="name"
+           type="text" 
+           value={this.state.name}
+           required 
+           onChange={this.handleInputChange}
+           />
+          </div>
+           <div className="inputBox">
+           <label htmlFor="email">email: </label>
+           <input
+           name="email"
+           type="email" 
+           value={this.state.email} 
+           required
+           onChange={this.handleInputChange}
+           />
+           </div>
+           <div className="inputBox">
+           <label htmlFor="phoneNumber">Phone number: </label>
+           <input
+           name="phoneNumber"
+           type="tel" 
+           value={this.state.phoneNumber} 
+           required
+           onChange={this.handleInputChange}
+           />
+           </div>
+          </div>
+           <div className="button-bar">
+           <button type="submit">Save</button>
+           </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <p>{this.state.name}</p>
+      )
+    }
+
+    
   }
 }
 
