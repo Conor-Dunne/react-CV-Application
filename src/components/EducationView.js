24d68ea78c9004c51bf2ddedda3b6063fe945128
Conor <conordunne@outlook.ie>
2022-12-28 04:58:37 +0000

    Add css styling

--- src/components/EducationView.js
+++ src/components/EducationView.js
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
 
 class EducationView extends Component {
-   
-    
+  render() {
+    return (
+      <div className="layout">
+        <h1
+         style={{ display: !this.props.inPreview ? "none" : "block" }}
+        >
+          <u>Education:</u>
+        </h1>
+        {this.props.institutes.map((institute) => {
+          return (
+            <div className="item">
+              <h3>{institute.name}</h3>
+              <h3>{institute.course}</h3>
+              <p>{`${institute.startdate} to ${institute.endDate}`}</p>
+              <div className="button-bar">
+              <button
+                className="delete-btn"
+                onClick={() => this.props.onDelete(institute.id)}
+                style={{ display: this.props.inPreview ? "none" : "block" }}
+              >
+                Delete
+              </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
-    render() {
-        return (
-            
-                <div  className="layout">
-                    <h1><u>Education:</u></h1>
-                    {this.props.institutes.map((institute) => {
-                    return (
-                        <div  className="item">
-                            <h3>{institute.name}</h3>
-                            <h3>{institute.course}</h3>
-                            <p>{`${institute.startdate} to ${institute.endDate}`}</p>
-                            <button 
-                            onClick={() => this.props.onDelete(institute.id)} 
-                            style={{ display: this.props.inPreview ? 'none' : 'block' }}
-                            >Delete
-</button>
-                        </div>
-                    )
-                })}
-                </div>
-            
-        );
-    }
-    
-};
-
-
-
-export default EducationView
+export default EducationView;
